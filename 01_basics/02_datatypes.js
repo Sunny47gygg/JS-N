fd096887fccf232ab1ce0fa48dd7eb1a7e07ced2
Sunny47gygg <101053527+Sunny47gygg@users.noreply.github.com>
2025-01-22 12:01:24 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) we are using nodejs, not browser
+console.log(3+3)
+// code readability should be high
+
+console.log("sunny")
+
+let Name = "sunny"
+let age = 21
+let isLoggedIn = true
+
+//number => 2 to power 53
+//BigInt => for bigger digits than number
+//string => ""
+//boolean => true/false
+//null => standalone value/ representation of empty value
+//undefined => declared but not assigned
+//symbo => unique
+
+//object
+
+console.log(typeof "sunny")
+console.log(typeof age)
+console.log(typeof null);
+console.log(typeof undefined)
