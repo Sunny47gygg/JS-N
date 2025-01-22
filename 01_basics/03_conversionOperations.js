c4f69c285d1f3ffd720cca13e4296556ff995437
Sunny47gygg <101053527+Sunny47gygg@users.noreply.github.com>
2025-01-22 12:37:25 +0000

    container config done

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+let score = false
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// 33 =>number
+// "33abc" , undefined => NaN
+//null => 0
+//true=>1 ; false=>0
+
+let isLoggedIn = "sunnyy"
+let booleanisLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanisLoggedIn);
+
+//1 => true ; 0 => false
+//"", null, undefined => false
+//"bhfe" => true
+
+let somenumber = 33
+let stringNumber = String(somenumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
