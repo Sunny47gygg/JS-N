73c5cf57183749663b10b6f03457cdc016bce7b6
Sunny47gygg <101053527+Sunny47gygg@users.noreply.github.com>
2025-01-31 15:17:25 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,36 @@
+const name = "sunny"
+const repoCount = 40
+
+//console.log(name + repoCount + "high");
+
+console.log(`my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('sunny-dagia-com')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('n'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+const newStringOne = "   sunny   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://sunny.com/sunny%20dagia"
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('sadia'));
+
+console.log(gameName.split('-'));
+
+
+
+
